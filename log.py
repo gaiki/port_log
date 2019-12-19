3c6cf3a0ea824997bdf655344252612b38076928
gaiki <git@gaiki.pl>
2019-12-20 00:11:49 +0100

    add datetime as string

--- log.py
+++ log.py
@@ -1,7 +1,10 @@
+import datetime
 import socket	#for sockets
 import sys	#for exit
 import subprocess
 
+
+now = datetime.datetime.now().isoformat(' ', 'seconds')
 s = socket.socket()
 host = '0.0.0.0'
 port = 22
@@ -12,6 +15,6 @@ s.listen(5)
 while True:
     c, addr = s.accept()
     print (addr[0])
-    eko = 'echo ' + addr[0] + ' >> /home/log.txt'
+    eko = 'echo ' + addr[0] + ' ' + now + ' >> /home/log.txt'
     subprocess.run(eko, shell=True)
     c.close()
